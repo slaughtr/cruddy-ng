9a2dd35c259a34d59dc0b545658b26ca2303352b
Dallas <dslaughtr@gmail.com>
2017-05-11 10:25:54 -0700

    Add boilerplate new thing form, no submit action yet

--- src/app/create-form/create-form.component.ts
+++ src/app/create-form/create-form.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
+import { DatabaseService } from '../database.service'
 
 @Component({
   selector: 'app-create-form',
   templateUrl: './create-form.component.html',
   styleUrls: ['./create-form.component.css']
 })
-export class CreateFormComponent implements OnInit {
 
-  constructor() { }
+export class CreateFormComponent implements OnInit {
+  things: FirebaseListObservable<any[]>
+  rooms: FirebaseListObservable<any[]>
+  boxes: FirebaseListObservable<any[]>
+
+  constructor(private dbService: DatabaseService) {}
 
   ngOnInit() {
+    this.things = this.dbService.getThings()
+    this.boxes = this.dbService.getBoxes()
+    this.rooms = this.dbService.getRooms()
   }
 
 }
--- src/app/display/display.component.ts
+++ src/app/display/display.component.ts
@@ -2,12 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { DatabaseService } from '../database.service'
 
-
 @Component({
   selector: 'app-display',
   templateUrl: './display.component.html',
   styleUrls: ['./display.component.css']
 })
+
 export class DisplayComponent implements OnInit {
   things: FirebaseListObservable<any[]>
   rooms: FirebaseListObservable<any[]>
